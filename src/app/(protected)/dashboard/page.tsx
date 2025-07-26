201d381ccb58124005905967c1fba0107b53e177
Luky Ramdhani <luky.ramdhanie@gmail.com>
2025-07-26 17:32:14 +0700

    📦️ Add Schema db
    add schema db

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+const Page = async (props: Props) => {
+  // user async because we want to fetch data from the server
+
+  return <div>page</div>;
+};
+
+export default Page;
